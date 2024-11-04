72f0457cfb58a7a0a1a3ec2c561314e191980a0b
Xx9810 <montanobraulio36@gmail.com>
2024-11-04 15:10:54 -0600

    Add files via upload

---
+++ so/script.js
@@ -0,0 +1,15 @@
+let currentIndex = 0;
+const slides = document.querySelectorAll(".rotador .slide");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle("active", i === index);
+  });
+}
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % slides.length;
+  showSlide(currentIndex);
+}
+
+setInterval(nextSlide, 3000);
